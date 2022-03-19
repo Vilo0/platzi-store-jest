f42c3b5bb05f50da4c183253a921f16607c67d2b
candidato_evilospalacios <thevilos@gmail.com>
2022-03-19 14:52:24 -0300

    clase 10

--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { create } from 'react-test-renderer';
 import ProviderMock from '../../__mocks__/ProviderMock';
 import Header from '../../components/Header';
 
@@ -12,5 +13,12 @@ describe('<Header />', () => {
   test('Render del Titulo', () => {
     const header = mount(<ProviderMock><Header /></ProviderMock>);
     expect(header.find('.Header-title').text()).toEqual('Platzi Store');
+  });
+});
+
+describe('Header Snapshot', () => {
+  test('Comprobar el Snapshot del header', () => {
+    const header = create(<ProviderMock><Header /></ProviderMock>);
+    expect(header.toJSON()).toMatchSnapshot();
   });
 });
